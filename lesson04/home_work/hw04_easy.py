d5a28d725c143d7f3bcf3f24a79273d11272b906
Petr <petr-sed@rambler.ru>
2019-01-24 13:58:03 +0300

    easy 3

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -17,7 +17,7 @@ print(new_list)
 # Получить список фруктов, присутствующих в обоих исходных списках.
 
 list_1 = ["яблоко", "апельсин", "мандарин", "лимон"]
-list_2 = ["мандарин", "яблоко", "слива", ":груша"]
+list_2 = ["мандарин", "яблоко", "слива", "груша"]
 
 new_list = []
 for i in range(len(list_1)):
@@ -31,3 +31,8 @@ print(new_list)
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+new_list=[]
+for i in range (len(old_list)):
+    if old_list[i] > 0 and old_list[i]%3 is 0 and old_list[i]%4 > 0:
+        new_list.append(old_list[i])
+print(new_list)
