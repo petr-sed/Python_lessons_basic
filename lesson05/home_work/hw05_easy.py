dae2d6a5672e42bf7764ef45761224a35edbfaad
Petr <petr-sed@rambler.ru>
2019-01-28 14:21:22 +0300

    Создал функцию создания и удаления папок

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -4,29 +4,46 @@
 # И второй скрипт, удаляющий эти папки.
 import os
 
-dir_path = os.getcwd()
-list_dirs = os.listdir(dir_path)
+def make_dir(name, stat, path = os.getcwd()):
+    list_dirs = os.listdir(path)
+    if name in list_dirs:
+        if stat is "stat_del":
+            os.rmdir(name)
+            print("Папка {} обнаружена и удалена".format (name))
+        elif stat is "stat_write":
+            print("Папка {} уже сущеcтвует".format(name))
+    else:
+        if stat is "stat_write":
+            os.mkdir(name)
+            print("Папка {} создана".format(name))
+        elif stat is "stat_del":
+            print("Папка {} не сущеcтвует".format(name))
+
+path = os.getcwd()
+list_dirs = os.listdir(path)
 
 for i in range(1, 10):
     dir_nm = "dir_%s" % i
     if dir_nm in list_dirs:
-        os.rmdir(dir_nm)
+        make_dir(dir_nm, "stat_del")
     else:
-        os.mkdir("dir_%s" % i)
+        make_dir(dir_nm, "stat_write")
+
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-
+"""
 print(list_dirs)
 list_new = list(item for item in list_dirs if item.find('.') is -1)
 print(list_new)
-
+"""
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 
-
+"""
 script_nm = os.path.basename(__file__)
 script_nm = os.path.join(dir_path, script_nm)
+
 script_copy = script_nm[0: script_nm.find('.')]+ '_copy.py'
 content = ''
 with open(script_nm, 'r', encoding='UTF-8') as f:
@@ -34,7 +51,7 @@ with open(script_nm, 'r', encoding='UTF-8') as f:
 with open(script_copy, 'w', encoding='UTF-8')as f:
     for index in content:
         f.write(index + '\n')
-
+"""
 
 
 
