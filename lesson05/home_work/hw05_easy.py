b16fd498745c0f43e4182a898f123d479802d3c0
Petr <petr-sed@rambler.ru>
2019-01-28 12:45:15 +0300

    easy 2

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,10 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 import os
-dir_path = os.path.join(os.getcwd())
+
+dir_path = os.getcwd()
 list_dirs = os.listdir(dir_path)
+
 for i in range(1, 10):
     dir_nm = "dir_%s" % i
     if dir_nm in list_dirs:
@@ -15,5 +17,10 @@ for i in range(1, 10):
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+print(list_dirs)
+list_new = list(item for item in list_dirs if item.find('.') is -1)
+print(list_new)
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
