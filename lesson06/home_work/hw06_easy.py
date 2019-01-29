0f2933cc4db96dda3b24e5369643012d2d283192
Petr <petr-sed@rambler.ru>
2019-01-29 16:06:44 +0300

    Написал функции  рассчета периметра, высоты и площади

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,6 +1,8 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 import math
+
+
 class TrEug:
     def __init__(self, point_a: list, point_b: list, point_c: list):
         self.point_a = point_a
@@ -15,23 +17,43 @@ class TrEug:
         if  self.point_c[0] > self.point_b[0]:
             self.point_b, self.point_c = self.point_c, self.point_b
 
-        print(self.point_a)
-        print(self.point_b)
-        print(self.point_c)
-
     def sides(self):
+        TrEug.orient(self)
         side = lambda p1, p2 : math.sqrt((int(p1[0]) - int(p2[0]))**2 + (int(p1[1]) - int(p2[1]))**2)
-        side_a = side(self.point_a, self.point_b)
-        side_b = side(self.point_b, self.point_c)
-        side_c = side(self.point_c, self.point_a)
-        print(side_a)
-        print(side_b)
-        print(side_c)
+        r_side = lambda r : round(r, 2)
+        side_a = r_side(side(self.point_a, self.point_b))
+        side_b = r_side(side(self.point_b, self.point_c))
+        side_c = r_side(side(self.point_c, self.point_a))
+
+        return side_a, side_b, side_c, 2
+
+
+    def perimetr(self):
+        list_sides = list(TrEug.sides(self))
+        per = list_sides[0] + list_sides[1] + list_sides[2]
+
+        return per
+
+
+    def visota(self):
+        list_sides = list(TrEug.sides(self))
+        p = TrEug.perimetr(self)/2
+        vis =2/list_sides[1] * math.sqrt(p*(p-list_sides[0])*(p-list_sides[1])*(p-list_sides[2]))
+
+        return round(vis, 2)
+
+    def face(self):
+        list_sides = list(TrEug.sides(self))
+        v = TrEug.visota(self)
+        f = (list_sides[1]*v)/2
+
+        return round(f,2)
 
 TrEug_1 = TrEug([-2, -2], [1, 1], [2, -3])
-TrEug_1.orient()
-TrEug_1.sides()
 
+print(TrEug_1.perimetr())
+print(TrEug_1.visota())
+print(TrEug_1.face())
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
