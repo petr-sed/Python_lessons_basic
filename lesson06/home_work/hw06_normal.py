315a63ef3270af22247d85e7e4a7b6a49e8fbf0f
Petr <petr-sed@rambler.ru>
2019-01-31 22:09:38 +0300

    разобрался с записью

--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -22,11 +22,36 @@ class Klass:
         self.name_kl = name_kl
 
     def add_klass(self, lessons :list):
-        lessons = str(lessons)
-        return "Klass added: "+lessons
+        with open("klass.txt", 'r') as lst_lines:
+           lines = []
+           for line in lst_lines:
+               lines.append(line[:-1])
+           print(lines)
+           point =0
+           for i in lines:
+               print(i)
+               if self.name_kl in i:
+                   print(i)
+                   point = 1
+           if point == 0:
+               print("not")
 
-    def klass_info(self, param):
-        return "Klass info"+param
+
+"""
+                  st_i = list(i.split(','))
+
+
+
+                  #print(st_i)
+               else:
+                   str_tmp = "{}, ".format(self.name_kl)
+                   for s in lessons:
+                       str_tmp = str_tmp + "{} : ,".format(s)
+                   lines.append(str_tmp[:-1])
+
+"""
+    #def klass_info(self, param):
+     #   return "Klass info"+param
 
 class Stud (Klass):
     def add_stud(self, name_stud, parents_stud):
@@ -39,3 +64,6 @@ class Teach (Klass):
     def add_teach(self, teach_name, lesson):
         add_string = "{}, {}, {}".format(self, lesson, teach_name)
         return add_string
+
+klass_1 = Klass("8h")
+klass_1.add_klass(["trud", "russkiy"])
